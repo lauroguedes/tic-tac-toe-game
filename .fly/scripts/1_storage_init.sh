e783c394f7745e9be26cb5f90c9230408c19ced2
Lauro Guedes <leowgweb@gmail.com>
2024-10-26 15:34:41 +0100

    conf: Remove fly io integration

--- .fly/scripts/1_storage_init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-FOLDER=/var/www/html/storage/app
-if [ ! -d "$FOLDER" ]; then
-    echo "$FOLDER is not a directory, copying storage_bkp content to storage"
-    cp -r /var/www/html/storage_bkp/. /var/www/html/storage
-    echo "deleting storage_bkp..."
-    rm -rf /var/www/html/storage_bkp
-fi
